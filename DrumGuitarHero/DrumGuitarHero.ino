c6120e4f50e35ce7116246b8c20d639927f86abb
Lider <159666594+ldgd2@users.noreply.github.com>
2024-02-14 12:06:36 -0400

    Update DrumGuitarHero.ino

--- DrumGuitarHero/DrumGuitarHero.ino
+++ DrumGuitarHero/DrumGuitarHero.ino
@@ -1,20 +1,20 @@
 #include "UnoJoy.h"
 
-#define PIEZO_THRESHOLD 50 // Umbral de sensibilidad del piezoeléctrico, ajusta según sea necesario
-#define BUTTON_THRESHOLD 800 // Umbral para los botones pulsadores, ajusta según sea necesario
+#define PIEZO_THRESHOLD 50      //  Umbral de sensibilidad del piezoeléctrico, ajusta según sea necesario - Piezo sensitivity threshold, adjust as necessary
+#define BUTTON_THRESHOLD 800    //  Umbral para los botones pulsadores, ajusta según sea necesario - Threshold for push buttons, adjust as needed 
 
 // Definición de los pines para los botones y los sensores
-#define TRIANGLE_PIEZO_PIN A0    //Pad Rojo
-#define CIRCLE_PIEZO_PIN A1     //Pad Amarillo
-#define SQUARE_PIEZO_PIN A2     //Pad Azul
-#define CROSS_PIEZO_PIN A3      //Pad Naranja
-#define L1_PIEZO_PIN A4         //Pad Verde
+#define TRIANGLE_PIEZO_PIN A0    // Pad Rojo  - Red Pad
+#define CIRCLE_PIEZO_PIN A1      // Pad Amarillo - Yellow Pad
+#define SQUARE_PIEZO_PIN A2      // Pad Azul - Blue Pad
+#define CROSS_PIEZO_PIN A3       // Pad Naranja - Orange Pad
+#define L1_PIEZO_PIN A4          // Pad Verde - Green Pad
 
-#define DPAD_UP_BUTTON_PIN 2    //Arriba
-#define DPAD_DOWN_BUTTON_PIN 3  //Abajo
-#define START_BUTTON_PIN 4     //start
-#define SELECT_BUTTON_PIN 5    //Select
-#define L3_BUTTON_PIN 6       //Pedal
+#define DPAD_UP_BUTTON_PIN 2         // Arriba - Up
+#define DPAD_DOWN_BUTTON_PIN 3     // Abajo - Down
+#define START_BUTTON_PIN 4        // start
+#define SELECT_BUTTON_PIN 5      // Select
+#define L3_BUTTON_PIN 6          // Pedal
 
 void setup() {
   setupPins();
@@ -27,7 +27,8 @@ void loop() {
 }
 
 void setupPins(void) {
-  // Configurar pines digitales para los botones pulsadores como entradas con pull-up
+  // Configuración de los pines digitales para los botones pulsadores como entradas pull-up 
+  // Configuring the digital pins for the push buttons as pull-up inputs
   pinMode(DPAD_UP_BUTTON_PIN, INPUT_PULLUP);
   pinMode(DPAD_DOWN_BUTTON_PIN, INPUT_PULLUP);
   pinMode(START_BUTTON_PIN, INPUT_PULLUP);
@@ -39,6 +40,7 @@ dataForController_t getControllerData(void) {
   dataForController_t controllerData = getBlankDataForController();
 
   // Leer los valores de los pines analógicos para los sensores piezoeléctricos
+  // Read the values of the analog pins for the piezoelectric sensors
   int trianglePiezoValue = analogRead(TRIANGLE_PIEZO_PIN);
   int circlePiezoValue = analogRead(CIRCLE_PIEZO_PIN);
   int squarePiezoValue = analogRead(SQUARE_PIEZO_PIN);
@@ -46,6 +48,7 @@ dataForController_t getControllerData(void) {
   int l1PiezoValue = analogRead(L1_PIEZO_PIN);
 
   // Verificar si se supera el umbral y establecer el estado de los botones correspondientes
+  // Check if the threshold is exceeded and set the status of the corresponding buttons
   if (trianglePiezoValue >= PIEZO_THRESHOLD) {
     controllerData.triangleOn = true;
   }
@@ -63,6 +66,7 @@ dataForController_t getControllerData(void) {
   }
 
   // Leer el estado de los botones pulsadores y establecer el estado correspondiente
+  // Read the status of push buttons and set the corresponding status
   if (digitalRead(DPAD_UP_BUTTON_PIN) == LOW) {
     controllerData.dpadUpOn = true;
   }
